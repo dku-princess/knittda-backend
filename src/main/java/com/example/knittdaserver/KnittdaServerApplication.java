77a931cd7314631b5c4a592b8672f7cc68812cc1
이지수 <jisoolee031@gmail.com>
2025-03-17 11:11:51 +0900

    init: 프로젝트 초기 세팅(Spring Boot + Docker + MySQL)

---
+++ src/main/java/com/example/knittdaserver/KnittdaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.knittdaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnittdaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnittdaServerApplication.class, args);
+    }
+
+}
